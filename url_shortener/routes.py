2cc04ae8329be859a9ba2f7d7c724512274ec726
Kunal Sharma <mailkunalsharma99@gmail.com>
2021-01-04 21:47:47 +0530

    allowed alphanumeric in custom_end and remove spaces in custom_url
    Added functionality to allow alphanumeric characters, -, _ only and remove spaces in custom_url

--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -39,17 +39,28 @@ def add_link():
     original_url = request.form.get('original_url')
     custom_end = request.form.get('custom_end')      
     
+    #start input validation
     if original_url is None:
         return "original_url field is required", 400
 
     if not UrlValidator.validate(original_url):
         return "Invalid url", 400
     
+    #remove all spaces in custom_end
+    custom_end = custom_end.replace(" ", "")
+
     if len(custom_end) > 64:
         return "custom_url is longer than 64 characters", 400      
     
     if len(original_url) > 512:
-        return "original_url is longer than 512 characters", 400      
+        return "original_url is longer than 512 characters", 400     
+
+    #allow alphanumeric characters, -, _ only in custom_end
+    regex = '[a-zA-z0-9_-]$'
+    
+    if((custom_end is not None) and (not re.search(regex, custom_end))):
+        return "custom_url contains invalid characters", 400
+    #end input validation     
 
     already_exists = Link.query.filter_by(short_url=custom_end).first()
 
